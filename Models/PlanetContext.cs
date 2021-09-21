901ced0f6516537ed3266a4bd4d37ab0e2248156
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2021-09-21 18:33:56 +0200

    make new controller for planets

---
+++ Models/PlanetContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace gttgBackend.Models
+{
+    public class PlanetContext : DbContext
+    {
+        public PlanetContext(DbContextOptions<PlanetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PlanetData> PLanetList { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using gttgBackend.Models;
 
 namespace gttgBackend
 {
@@ -32,6 +34,9 @@ namespace gttgBackend
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "gttgBackend", Version = "v1" });
             });
+
+            services.AddDbContext<PlanetContext>(opt =>
+                                               opt.UseInMemoryDatabase("PlanetList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
