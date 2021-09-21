72940be3fd419c6ddf5fea1143bda64ba0bd7bb0
MarkonLehel <lehel.markon@gmail.com>
2021-09-21 10:36:54 +0200

    Add some data files

---
+++ Modells/PlanetData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gttgBackend.Modells
+{
+    public struct PlanetData
+    {
+        public int PlanetID { get; }
+        public string PlanetName { get; set; }
+        public string PlanetDescription { get; set; }
+        public List<LodgingData> Lodgings { get; }
+
+
+    }
+}
