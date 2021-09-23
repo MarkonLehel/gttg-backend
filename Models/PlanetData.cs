15602feb3f3890ebdf568bd8bce047d2ea5165e5
MarkonLehel <lehel.markon@gmail.com>
2021-09-23 20:43:16 +0200

    Add image to planetdata

--- Models/PlanetData.cs
+++ Models/PlanetData.cs
@@ -5,7 +5,7 @@ namespace gttgBackend.Models
     public class PlanetData
     {
         public static List<PlanetData> planetList = new();
-        public PlanetData(string planetName, string planetDescription, int population, string race, int x,int y)
+        public PlanetData(string planetName, string planetDescription, int population, string race, string imageName, int x,int y)
         {
             X = x;
             Y = y;
@@ -16,12 +16,14 @@ namespace gttgBackend.Models
             PlanetName = planetName;
             PlanetDescription = planetDescription;
             planetList.Add(this);
+            ImageName= imageName;
         }
 
         private static int currentID = 1;
         public int PlanetDataID { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
+        public string ImageName { get; set; }
         public int Population { get; set; }
         public string Race { get; set; }
         public string PlanetName { get; set; }
