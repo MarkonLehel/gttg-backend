3c49cfd772538badf133dcba416cf2996fd8a830
MarkonLehel <lehel.markon@gmail.com>
2021-10-18 16:26:36 +0200

    Add event, traveltype and lodging seed to database

--- Migrations/20211018142427_Seeds.Designer.cs
+++ Migrations/20211018142427_Seeds.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using gttgBackend.Models;
 
 namespace gttgBackend.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211018142427_Seeds")]
+    partial class Seeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
