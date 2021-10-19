759238799e3d8852a97baed415313eb3a8de8b95
MarkonLehel <lehel.markon@gmail.com>
2021-10-19 11:47:44 +0200

    Create TripDataRepo interface and migrate new TripData model to DB

---
+++ Migrations/20211019094532_AddUserToTripDataModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace gttgBackend.Migrations
+{
+    public partial class AddUserToTripDataModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "TripList",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "TripList");
+        }
+    }
+}
--- Models/Interfaces/ITripDataRepository.cs
+++ Models/Interfaces/ITripDataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace gttgBackend.Models.Interfaces
@@ -6,6 +7,11 @@ namespace gttgBackend.Models.Interfaces
     interface ITripDataRepository
     {
         Task<ActionResult<TripData>> GetTripById(int id);
-        Task<ActionResult<EventData>> GetTrips(int id);
+        Task<ActionResult<IEnumerable<TripData>>> GetTrips();
+
+        Task<ActionResult<TripData>> AddTrip(TripData trip);
+
+        Task<ActionResult<TripData>> RemoveTrip(int id);
+
     }
 }
